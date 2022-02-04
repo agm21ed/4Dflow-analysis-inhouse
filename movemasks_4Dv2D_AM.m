243470fd47f823fe5cc6f0957d234d0bb79ae814
Al Morgan <43786406+agm21ed@users.noreply.github.com>
2022-02-04 11:21:32 +0000

    Update movemasks_4Dv2D_AM.m

--- movemasks_4Dv2D_AM.m
+++ movemasks_4Dv2D_AM.m
@@ -1,10 +1,12 @@
+% simple code to copy the masks drawn around vessel lumens in FSLeyes to a masks directory named ROIs
+% make sure to create the ROIs folder manually in the 4D analysis directory (or have the code create it just once)
+
 setFlowParameters_4Dv2D_AM;
 load('allFlowOptions');
-load([procRoot '4Dv2Danalysis/scanInfo.mat']); %point to wherever scaninfo is
+load(%point to wherever scaninfo.mat is)
 
 spm('defaults', 'FMRI'); spm_jobman('initcfg');
 
-% for iSubject=6:scanInfo.N
 for iSubject=[11]
     for iFlowScan=7:10%1:13% 1.RMCA 2.LMCA 3.RACA 4.LACA 5.RPCA 6.LPCA 7.SSS 8.StS 9.RTS 10.LTS 11.RICA 12.LICA 13.BA
         
